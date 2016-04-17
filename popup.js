2644424af8b069f02ce3393103fe529880c94891
Longfei Li <codyli520@gmail.com>
2016-04-17 14:05:00 -0700

    entension working, but need to think better randomizer

--- popup.js
+++ popup.js
@@ -1,54 +1,67 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo, 6000);
-      done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
-}
-
+//API key
 var APIKEY = 'AIzaSyAUCR4AiwEbGSYwIjFyzWoqxXJ8ciQhfsE'
-//GET https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&publishedBefore=2016-01-01T00%3A00%3A00Z&q=trailer&type=video&key={YOUR_API_KEY}
+
+//Generate random date
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+function ISODateString(d){
+ function pad(n){return n<10 ? '0'+n : n}
+ return d.getUTCFullYear()+'-'
+      + pad(d.getUTCMonth()+1)+'-'
+      + pad(d.getUTCDate())+'T'
+      + pad(d.getUTCHours())+'%3A'
+      + pad(d.getUTCMinutes())+'%3A'
+      + pad(d.getUTCSeconds())+'Z'
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
 
   var startButton = document.getElementById('start');
 
   startButton.addEventListener('click', function() {
-      document.getElementsByTagName("iframe")[0].setAttribute("src","https://www.youtube.com/embed/UNAa5-uCowY");
-      var player;
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player('player', {
-              height: '390',
-              width: '640',
-              videoId: 'UNAa5-uCowY',
-              events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-              }
-            });
-        }
+      
+      //random date in RFC3339 2010-01-01T00:00:00:Z format
+      var dateBefore = randomDate(new Date(2012, 0, 1), new Date());
+      var normalizeDate = ISODateString(dateBefore);
+      
+      console.log(normalizeDate);
+      
+      //random number 
+      var random= Math.floor((Math.random() * 50));
+      
+      console.log(random);
+      
+      var url = "";
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=viewCount&publishedBefore ="+normalizeDate+"&q=movie+trailer&safeSearch=strict&type=video&videoEmbeddable=true&fields=items(id%2Ckind%2Csnippet)&key="+APIKEY, true);
+      xhr.send();
+      xhr.onreadystatechange = processRequest;
+      
+      function processRequest(e){
+          if (xhr.readyState ==4 && xhr.status ==200){
+              var response = JSON.parse(xhr.responseText);
+              
+              console.log(response);
+              
+              var videoURL = "https://www.youtube.com/embed/";
+              videoURL = videoURL+response["items"][random]["id"]["videoId"];
+              document.getElementsByTagName("iframe")[0].setAttribute("src",videoURL);
+              var player;
+                function onYouTubeIframeAPIReady() {
+                    player = new YT.Player('player', {
+                      height: '390',
+                      width: '640',
+                      videoId: 'UNAa5-uCowY',
+                      events: {
+                        'onReady': onPlayerReady
+                      }
+                    });
+                }
+          }
+      }
   });
     
 });
